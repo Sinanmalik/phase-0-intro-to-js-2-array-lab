28930f30fe44fba9eebb755857e9988399349988
Sinanmalik <sinanmalik3113@gmail.com>
2024-02-12 20:10:14 -0500

    complete lab

--- index.js
+++ index.js
@@ -18,26 +18,22 @@ function destructivelyRemoveFirstCat() {
 }
 
 function appendCat(name) {
-  // new array new cat appended
   return [...cats, name];
 }
 
 function prependCat(name) {
-  //new array with new cat prepended
   return [name, ...cats];
 }
 
 function removeLastCat() {
-  // new array w/t the last cat
   return cats.slice(0, -1);
 }
 
 function removeFirstCat() {
-  //new array without the first cat
   return cats.slice(1);
 }
 
-// Export the functions 
+
 module.exports = {
   cats,
   destructivelyAppendCat,
